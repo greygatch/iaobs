9d9191fdc3216bdd7403c48ada39a808b4083fc7
Edwin Dover <edwin.dover@changehealthcare.com>
2019-03-28 14:56:50 -0500

    Fix bug

--- app.js
+++ app.js
@@ -151,11 +151,11 @@ function formatTimer(time) {
 
 // Return human readable date string.
 function getCurrentDate() {
+  const date = new Date();
+
   let day = date.getDate();
   let month = date.getMonth()+1;
-
-  const date = new Date();
-  const year = date.getFullYear();
+  let year = date.getFullYear();
 
   day < 10 ? day = `0${day}` : null;
   month < 10 ? month = `0${month}` : '';
